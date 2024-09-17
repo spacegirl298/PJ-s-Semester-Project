bd75f935fa748f72c386e845deec90ed9df93bf1
padifalas <padifalas@outlook.com>
2024-09-18 00:31:01 +0200

    Phone Message
    * When player is within radius of phone; a panel with the screenshot appears.

--- Assets/Scripts/Puzzles/PhoneMessage.cs
+++ Assets/Scripts/Puzzles/PhoneMessage.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-
 public class PhoneMessage : MonoBehaviour
 {
     public Transform player; 
     public float distanceFromPhone = 3f; //radius
     public GameObject messagePanel; 
-    
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         messagePanel.SetActive(false); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         float distance = Vector3.Distance(player.position, transform.position);
         if (distance < distanceFromPhone)
@@ -31,6 +28,7 @@ public class PhoneMessage : MonoBehaviour
             messagePanel.SetActive(false); 
         }
     }
+
     
     private void OnDrawGizmos()
     {
